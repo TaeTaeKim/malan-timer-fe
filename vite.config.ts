5ab9de6f5da2a5107734c6d5fe4a1d7de40c55ef
김태윤 <mygod456@kakao.com>
2025-07-13 18:40:37 +0900

    <feat> : Vite configuration update to support production base path and improve server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/extract": {
-        target: "http://39.115.66.158:38000", // AI 서버 주소로 변경 가능
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  let base = "/";
+  if (mode === "production") {
+    base = "/malan-timer/";
+  }
+
+  return {
+    plugins: [vue()],
+    base,
+    server: {
+      proxy: {
+        "/extract": {
+          target: "http://39.115.66.158:38000", // AI 서버 주소로 변경 가능
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
